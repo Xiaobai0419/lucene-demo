fc0f861d51236ca1c794e2a5a82820ef1ca86507
Xiaobai0419 <minexiaobai@hotmail.com>
2018-10-03 15:04:27 +0800

    添加过滤器测试

--- src/main/java/com/xiaobai/lucene/crud/curd/test/TestQuery.java
+++ src/main/java/com/xiaobai/lucene/crud/curd/test/TestQuery.java
@@ -298,6 +298,35 @@ public class TestQuery {
         }
     }
 
+    /**
+     * 过滤
+     * @throws IOException
+     */
+    @Test
+    public void testFilterQuery() throws IOException {
+        /**
+         * 1:需要根据那个字段进行过滤
+         * 2:字段对应的最小值
+         * 3:字段对应的最大值
+         * 4:是否包含最小值
+         * 5：是否包含最大值...
+         */
+        // filter 是一个抽象类，定义不同的filter 相当于是不同的过滤规则...
+        Filter filter = NumericRangeFilter
+                .newIntRange("id", 8, 100, false, true);
+        TopDocs topDocs = indexSearcher.search(query, filter, 100);
+        System.out.println("总记录数：" + topDocs.totalHits);
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            System.out.println("相关度得分："+scoreDoc.score);//不影响相关度，仍按相关度高低降序展示
+            Document document = indexSearcher.doc(scoreDoc.doc);
+            System.out.println(document.get("id"));
+            System.out.println(document.get("title"));
+            System.out.println(document.get("content"));
+            System.out.println(document.get("author"));
+            System.out.println(document.get("link"));
+        }
+    }
+
     public static IndexWriter getIndexWriter() {
         return indexWriter;
     }
