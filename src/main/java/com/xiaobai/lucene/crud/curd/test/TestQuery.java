9c6b1a2549a97ab16d3fb92bcdc490cd1b764e1f
Xiaobai0419 <minexiaobai@hotmail.com>
2018-10-03 14:39:16 +0800

    添加排序测试

--- src/main/java/com/xiaobai/lucene/crud/curd/test/TestQuery.java
+++ src/main/java/com/xiaobai/lucene/crud/curd/test/TestQuery.java
@@ -2,7 +2,7 @@ package com.xiaobai.lucene.crud.curd.test;
 
 import com.xiaobai.lucene.crud.curd.util.LuceneUtils;
 import com.xiaobai.lucene.crud.curd.util.Type;
-import org.apache.lucene.document.Document;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
@@ -130,7 +130,8 @@ public class TestQuery {
 //        query = buildQuery(Type.RANGE);
 //        query = buildQuery(Type.REGEX);
 //        query = buildQuery(Type.TERM);
-        query = buildBooleanQuery();
+//        query = buildBooleanQuery();
+        query = buildWeightingQuery();
     }
 
     @Test
@@ -180,6 +181,121 @@ public class TestQuery {
 //        Query query3 = queryParser.parse("天下");
         query.add(query3, BooleanClause.Occur.MUST);//相当于sql语句的and
         return query;
+    }
+
+    /**
+     * 权重索引
+     */
+    @Test
+    public void testAddWeightingIndex() {//每次不是从头创建索引，而是在已有的索引目录增量添加
+        try {//如果要重新建立索引，可用更新操作，先删除所有此前的，再从头创建添加
+            indexWriter.addDocument(buildWeighting());
+            indexWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private Document buildWeighting() {
+        Document document = new Document();
+        IntField idField = new IntField("id", 100, Field.Store.YES);
+        //StringField不进行分词（整体算一个）--------------------------------------->不分词的按整体查，所以可以查到"马化腾",注意这个！！！
+        StringField authoField = new StringField("author", "xiaobai", Field.Store.YES);
+        StringField linkField = new StringField("link", "www.baidu.com", Field.Store.YES);
+        //TextField进行分词
+        TextField titleField = new TextField("title", "国庆节，最近比较上火，深悟人生，特立箴言，还是努力不够。往者不可谏，来者犹可追。", Field.Store.YES);
+        //==============================================
+        //注意：这里可以添加权重值，默认是1f，添加之后检索的时候排名就会靠前
+        titleField.setBoost(4f);
+        TextField contentField = new TextField("content", "国庆节，农历8月23，离职休养月余。\n" +
+                "\n" +
+                "睹大学后生博客学识之高度，为人做学问之器量效率，学识深度与广博，人生经历与认知，深以为不如。\n" +
+                "\n" +
+                "忆己年余病笃，怠惰，迷拙，蹉跎，又弗如远甚。\n" +
+                "\n" +
+                "以至今日亦倦怠慵懒不思改悟，无忧患，徒慰己自欺，无才而傲物，以绝知耻而后勇之心，遂不能务实有效，奋起直追。\n" +
+                "\n" +
+                "深悟人生，特立箴言：\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "\n" +
+                "首先，承认天外有天，天生见不清越不过的高山\n" +
+                "\n" +
+                "\n" +
+                "其次，承认自己的智力水平，懒惰，和努力程度\n" +
+                "\n" +
+                "\n" +
+                "再次，肯定和坚守自己的努力，悟性和持之以恒\n" +
+                "\n" +
+                "\n" +
+                "最后，放弃嫉妒，勉力执着和自我安慰，快乐知足地奋斗和生活，温暖，自信，正能量！\n" +
+                "\n" +
+                "\n" +
+                "往者不可谏，来者犹可追。\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "西元2018.10.2，病笃家中，键盘手书。", Field.Store.YES);
+        contentField.setBoost(8f);
+        document.add(idField);
+        document.add(authoField);
+        document.add(linkField);
+        document.add(titleField);
+        document.add(contentField);
+        return document;
+    }
+
+    private Query buildWeightingQuery() throws ParseException {
+        //封装查询条件(使用BooleanQuery对象，连接多个查询条件)
+        BooleanQuery query = new BooleanQuery();
+
+        //条件一（所属单位）
+        //词条查询
+        TermQuery query1 = new TermQuery(new Term("content", "中"));//无法匹配"中国",只能单个字匹配
+        //Occur.SHOULD相当于or
+        query.add(query1, BooleanClause.Occur.MUST);//Occur.MUST相当于sql语句的and
+
+        //条件二（图纸类别）
+        //词条查询
+        Query query2 = NumericRangeQuery.newIntRange("id", 1, 100, true, true);
+        query.add(query2, BooleanClause.Occur.MUST);
+
+        //条件三（文件名称和文件描述）
+        //多个字段进行检索的时候，查询使用QueryPaser
+        //要是直接new QueryParser()，也可以，但是只能查询一个字段
+        // select * from table where title=? or content=?
+        QueryParser queryParser = new MultiFieldQueryParser(Version.LUCENE_CURRENT,new String[]{"title","content"},LuceneUtils.getAnalyzer());
+        Query query3 = queryParser.parse("国家");//匹配两个字段中有一个带"第一"的
+        query.add(query3, BooleanClause.Occur.MUST);//相当于sql语句的and
+        return query;
+    }
+
+    /**
+     * 排序
+     * @throws IOException
+     */
+    @Test
+    public void testSortQuery() throws IOException {
+        Sort sort = new Sort();
+        // 升序
+        SortField sortField=new SortField("id", SortField.Type.INT);
+        // 降序
+//        SortField sortField = new SortField("id", SortField.Type.INT,true);
+        // 设置排序的字段...
+        sort.setSort(sortField);
+        TopDocs topDocs = indexSearcher.search(query, 100, sort);
+        System.out.println("总记录数：" + topDocs.totalHits);
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            System.out.println("相关度得分："+scoreDoc.score);//前面设置了排序，相关度失效，这里返回NaN
+            Document document = indexSearcher.doc(scoreDoc.doc);
+            System.out.println(document.get("id"));
+            System.out.println(document.get("title"));
+            System.out.println(document.get("content"));
+            System.out.println(document.get("author"));
+            System.out.println(document.get("link"));
+        }
     }
 
     public static IndexWriter getIndexWriter() {
